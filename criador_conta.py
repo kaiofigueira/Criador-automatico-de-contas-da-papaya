b3fad2f3c17e510e5b75f6bffc66466985993629
Kaio <60208103+kaiofigueira@users.noreply.github.com>
2024-09-04 11:12:13 -0300

    adicionar WebDriverWait para garantir que o carregamento do elemento tenha sido feito para evitar erros

--- criador_conta.py
+++ criador_conta.py
@@ -32,13 +32,23 @@ class CriadorConta():
     def entrar_site(self, site):
         self.driver.get(site)
 
-    def cadastrar_conta_papaya(self):
+    def cadastrar_conta_papaya(self, timeout=20):
         self.entrar_site(self.site_papaya)
-        campo_usuario = self.driver.find_element(By.NAME, 'userid')
-        campo_email = self.driver.find_element(By.ID, 'equalEmail')
-        campo_confirma_email = self.driver.find_element(By.ID, 'confirmEmail')
-        campo_senha = self.driver.find_element(By.NAME, 'pwd')
-        campo_checkbox = self.driver.find_element(By.ID, 'custCheckbox')
+        campo_usuario = WebDriverWait(self.driver, timeout).until(
+            EC.presence_of_element_located((By.NAME, 'userid'))
+        )
+        campo_email = WebDriverWait(self.driver, timeout).until(
+            EC.presence_of_element_located((By.ID, 'equalEmail'))
+        )
+        campo_confirma_email = WebDriverWait(self.driver, timeout).until(
+            EC.presence_of_element_located((By.ID, 'confirmEmail'))
+        )
+        campo_senha = WebDriverWait(self.driver, timeout).until(
+            EC.presence_of_element_located((By.NAME, 'pwd'))
+        )
+        campo_checkbox = WebDriverWait(self.driver, timeout).until(
+            EC.presence_of_element_located((By.ID, 'custCheckbox'))
+        )
 
         usuario_conta = self.email_temporario.split('@')[0]
         campo_usuario.send_keys(usuario_conta)
@@ -51,7 +61,7 @@ class CriadorConta():
         # Chama a função de esperar resolução do CAPTCHA
         self.verificar_resolucao_captcha()
 
-        button = WebDriverWait(self.driver, 10).until(
+        button = WebDriverWait(self.driver, timeout).until(
             EC.element_to_be_clickable((By.XPATH, '/html/body/div[4]/div/div[2]/div/form/button'))
         )
 
@@ -60,11 +70,11 @@ class CriadorConta():
         
         self.usuario_conta = usuario_conta
 
-    def verificar_ipblock(self):
+    def verificar_ipblock(self, timeout=20):
         div_xpath = '//*[@id="toast-container"]/div/div'
         try:
             # Espera até que o div com o xpath especificado esteja presente na página
-            div_element = WebDriverWait(self.driver, 10).until(
+            div_element = WebDriverWait(self.driver, timeout).until(
                 EC.presence_of_element_located((By.XPATH, div_xpath))
             )
             # Verifica se o texto do elemento contém a mensagem de bloqueio
@@ -78,17 +88,17 @@ class CriadorConta():
             print("Mensagem de bloqueio de ip não apareceu dentro do tempo especificado.")
             return False
 
-    def verificar_try_again_captcha(self):
+    def verificar_try_again_captcha(self, timeout=20):
         try:
             # Espera até que o iframe esteja presente e alterna o contexto para ele
-            iframe = WebDriverWait(self.driver, 20).until(
+            iframe = WebDriverWait(self.driver, timeout).until(
                 EC.presence_of_element_located((By.XPATH, '/html/body/div[5]/div[4]/iframe'))
             )
             self.driver.switch_to.frame(iframe)
             
             try:
                 # Espera até que o elemento com a classe 'rc-doscaptcha-header-text' esteja presente dentro do iframe
-                captcha_header = WebDriverWait(self.driver, 20).until(
+                captcha_header = WebDriverWait(self.driver, timeout).until(
                     EC.presence_of_element_located((By.XPATH, '//div[contains(@class, "rc-doscaptcha-header-text")]'))
                 )
                 
@@ -113,9 +123,13 @@ class CriadorConta():
             #print("Tempo limite excedido ao tentar encontrar o iframe ou o elemento dentro do iframe.")
             return False
 
-    def obter_email_temporario(self):
+    def obter_email_temporario(self, timeout=20):
         self.entrar_site(self.site_email_temporario)
-        email_element = self.driver.find_element(By.ID, 'email-input')
+        
+        email_element = WebDriverWait(self.driver, timeout).until(
+            EC.presence_of_element_located((By.ID, 'email-input'))
+        )
+        
         email_temporario = email_element.get_attribute('value')
         print(f'Email gerado: {email_temporario}')
 
@@ -136,16 +150,18 @@ class CriadorConta():
         
         return False
 
-    def clicar_email(self):
+    def clicar_email(self, timeout=20):
         print("Clicando no e-mail")
         tbody_xpath = '/html/body/main/div[1]/div[3]/div[1]/table/tbody'
 
         if not self.verificar_recebimento_email(tbody_xpath):
             return False
 
-        tbody_element = self.driver.find_element(By.XPATH, tbody_xpath)
-        tr_elements = tbody_element.find_elements(By.XPATH, './tr')
+        tbody_element = WebDriverWait(self.driver, timeout).until(
+            EC.presence_of_element_located((By.XPATH, tbody_xpath))
+        )
 
+        tr_elements = tbody_element.find_elements(By.XPATH, './tr')
         for tr_element in tr_elements:
             try:
                 element_id = tr_element.get_attribute('id')
@@ -166,11 +182,13 @@ class CriadorConta():
             
         return False
 
-    def clicar_link(self):
+    def clicar_link(self, timeout=20):
         print("Clicando no link de confirmação de registro")
         try:
             shadow_host_xpath = '//*[@id="body"]'
-            shadow_host = self.driver.find_element(By.XPATH, shadow_host_xpath)
+            shadow_host = WebDriverWait(self.driver, timeout).until(
+                EC.presence_of_element_located((By.XPATH, shadow_host_xpath))
+            )
 
             script_shadow = """
                 var shadowHost = arguments[0];
@@ -209,9 +227,12 @@ class CriadorConta():
 
         return True
 
-    def validar_criacao_conta_papaya(self):
+    def validar_criacao_conta_papaya(self, timeout=10):
         print("Validando criação da conta no papaya")
-        body_element = self.driver.find_element(By.ID, "portal")
+
+        body_element = WebDriverWait(self.driver, timeout).until(
+            EC.presence_of_element_located((By.ID, "portal"))
+        )
         h2_elements = body_element.find_elements(By.XPATH, '//*[@id="sign-up-complete"]/div/h2')
 
         for h2_element in h2_elements:
@@ -226,7 +247,7 @@ class CriadorConta():
         
         return False
 
-    def verificar_resolucao_captcha(self, timeout=1200, check_interval=30):
+    def verificar_resolucao_captcha(self, timeout=1200):
         try:
             WebDriverWait(self.driver, timeout).until(
                 EC.frame_to_be_available_and_switch_to_it((By.CSS_SELECTOR, "iframe[title='reCAPTCHA']"))
@@ -237,33 +258,17 @@ class CriadorConta():
             )
 
             print("Resolvendo o reCAPTCHA")
-
-            # start_time = time.time()
-
+            
             while True:
                 if "recaptcha-checkbox-checked" in captcha.get_attribute("class"):
                     print("reCAPTCHA foi verificado com sucesso!")
-                    return True  # Retorna False se o CAPTCHA foi verificado com sucesso
-                
-                # elapsed_time = time.time() - start_time
-                # if elapsed_time > timeout:
-                #     raise TimeoutException("Tempo limite alcançado ou o reCAPTCHA não foi verificado dentro do tempo especificado.")
-
-                # if elapsed_time % check_interval < 1:
-                #     if self.verificar_try_again_captcha():
-                #         print("Captcha detectou 'Try again later'.")
-                #         return False  # Retorna True se "Try again later" foi detectado
+                    return True
                 
                 time.sleep(1)
 
         except TimeoutException:
             print("Tempo limite alcançado ou o reCAPTCHA não foi verificado dentro do tempo especificado.")
-            return False  # Considera como um "Try again later" se o tempo expirar
-        # except Exception as e:
-        #     print("te encontrei")
-        #     print("")
-        #     print(e)
-        #     return False
+            return False
         
         finally:
             self.driver.switch_to.default_content()
